b8734c8e3597aa473787ba46763c5a71605944c1
Thomas Baumann <tbthomas.baumann@gmail.com>
2022-02-14 16:32:18 -0500

    refactored the code

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -18,7 +18,7 @@ class GamesController < ApplicationController
     @result = game_result(word_exist, letters_include_word, user_input, letters)
 
     score = user_input.length if word_exist && letters_include_word
-    @total_score = total_score(word_exist, letters_include_word, score)
+    @total_score = total_score(score)
   end
 
   def reset
@@ -29,45 +29,21 @@ class GamesController < ApplicationController
   private
 
   def word_exist?(word)
-    url = "https://wagon-dictionary.herokuapp.com/#{word}"
-    serialized = URI.open(url).read
-    result = JSON.parse(serialized)
-    result['found']
+    JSON.parse(URI.open("https://wagon-dictionary.herokuapp.com/#{word}").read)['found']
   end
 
   def letters_include_word?(word, letters)
-    letters_array = letters.chars
-    result = true
-    word.chars.each do |l|
-      if letters_array.include?(l)
-        letters_array - [l]
-      else
-        result = false
-      end
-    end
-    result
+    select_letters = word.chars.select{ |l| letters.chars.include?(l) }
+    return true if select_letters.length == word.length
   end
 
   def game_result(word_exist, letters_include_word, user_input, letters)
-    if word_exist && letters_include_word
-      result = "CONGRATULATION! #{user_input} is an english word!"
-    elsif !letters_include_word
-      result = "Sorry, but #{user_input.capitalize} can't be build with #{letters.split('')}"
-    elsif !word_exist
-      result = "Sorry, but #{user_input.capitalize} doesn't seem to be an english word"
-    end
-    result
+    return "CONGRATULATION! #{user_input} is an english word!" if word_exist && letters_include_word
+    return "Sorry, but #{user_input.capitalize} can't be build with #{letters.split('')}" if !letters_include_word
+    return "Sorry, but #{user_input.capitalize} doesn't seem to be an english word" if !word_exist
   end
 
-  def total_score(word_exist, letters_include_word, score)
-    if word_exist && letters_include_word
-      if session[:score].nil?
-        session[:score] = score
-      else
-        session[:score] += score
-      end
-    end
-    session[:score] || 0
+  def total_score(score)
+    session[:score].nil? ? session[:score] = score : session[:score] += (score || 0)
   end
-
 end
